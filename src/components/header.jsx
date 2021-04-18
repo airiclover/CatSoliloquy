1ba89c32382932fcaece4fcfef74b17c222b8544
airi <airi-love-clover@rakuten.jp>
2021-04-19 02:08:51 +0900

    💄 ハンバーガーメニューUI更新

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -33,41 +33,44 @@ export function Header() {
         {({ open }) => (
           <>
             <Menu.Button className="pt-4 pb-2 pl-3 pr-6 cursor-pointer fixed right-0 focus:outline-none">
-              {open ? (
-                <Image
-                  src="/img/cross.svg"
-                  alt="menuIcon"
-                  width={24}
-                  height={24}
-                  loading="eager"
-                  priority
-                />
-              ) : (
-                <Image
-                  src="/img/menu.svg"
-                  alt="menuIcon"
-                  width={24}
-                  height={24}
-                  loading="eager"
-                  priority
-                />
-              )}
+              <Image
+                src="/img/menu.svg"
+                alt="menuIcon"
+                width={24}
+                height={24}
+                loading="eager"
+                priority
+              />
             </Menu.Button>
 
-            <Menu.Items className="bg-gray-50 w-3/6 h-screen py-4 z-10 text-center  fixed top-14 right-0  focus:outline-none lg:hidden">
-              {LINKS.map((link) => (
-                <Menu.Item key={link.title}>
-                  {/* <Link href={link.href}> */}
-                  <Link href="/">
-                    <a>
-                      <ul>
-                        <li className="py-3">{link.title}</li>
-                      </ul>
-                    </a>
-                  </Link>
-                </Menu.Item>
-              ))}
-            </Menu.Items>
+            {open && (
+              <div className="fixed top-0 right-0 bg-white w-3/5 h-screen z-10">
+                <Menu.Button className="pt-2 pl-3 pr-6 cursor-pointer fixed top-4 right-0 focus:outline-none">
+                  <Image
+                    src="/img/cross.svg"
+                    alt="menuIcon"
+                    width={24}
+                    height={24}
+                    loading="eager"
+                    priority
+                  />
+                </Menu.Button>
+                <Menu.Items className="mt-14 text-center   focus:outline-none">
+                  {LINKS.map((link) => (
+                    <Menu.Item key={link.title}>
+                      {/* <Link href={link.href}> */}
+                      <Link href="/">
+                        <a>
+                          <ul>
+                            <li className="py-3">{link.title}</li>
+                          </ul>
+                        </a>
+                      </Link>
+                    </Menu.Item>
+                  ))}
+                </Menu.Items>
+              </div>
+            )}
           </>
         )}
       </Menu>
