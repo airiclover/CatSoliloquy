020629a7678ce81987170c664fdc82a67413da6b
airi <airi-love-clover@rakuten.jp>
2021-04-22 22:59:00 +0900

    💄 Update UI

--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -16,28 +16,47 @@ import Link from "next/link";
 
 import axios from "axios";
 
-axios
-  .get(process.env.BASE_URL + "/rcms-api/1/blog")
-  // .get("https://jsonplaceholder.typicode.com/posts")
-  .then(function (response) {
-    console.log(response);
-  });
+// axios
+//   .get(process.env.BASE_URL + "/rcms-api/1/blog")
+//   .then(function (response) {
+//     console.log(response);
+//   });
 
-// https://jsonplaceholder.typicode.com/posts
-// export async function getServerSideProps() {
+// https://jsonplaceholder.typicode.com/posts/1
+// export async function getStaticProps() {
 //   const res = await fetch(process.env.BASE_URL + "/rcms-api/1/blog");
 //   const posts = await res.json();
 //   console.log(posts);
 //   return { props: { posts } };
 // }
 
+export async function getStaticProps() {
+  const res = await fetch(`${process.env.BASE_URL}/rcms-api/1/blog`);
+  const data = await res.json();
+  console.log(data);
+
+  return {
+    props: {
+      data,
+    },
+    revalidate: 60 * 60,
+  };
+}
+
 export default function Blog(props) {
+  console.log(props);
+  // const { lists } = props;
+
   return (
     <div>
       {/* {console.log(props.data)} */}
       {/* <Link href={}></Link> */}
       <div>blog</div>
-      {/* {props.post} */}
+      {props.data.list[0].ymd}
+      <br />
+      {props.data.list[0].subject}
+      <br />
+      {props.data.list[0].contents}
     </div>
   );
 }
