6821b3475ffce2d97881c9340da6aa9df10a5160
airi <airi-love-clover@rakuten.jp>
2021-04-23 01:45:38 +0900

    💄 Update UI

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,18 +1,13 @@
 import { Card } from "src/components/card";
 import { MainLayout } from "src/layouts/main";
 import { BlogLayout } from "src/layouts/blog";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <MainLayout>
-        <BlogLayout>
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-        </BlogLayout>
+        <BlogLayout></BlogLayout>
       </MainLayout>
     </>
   );
