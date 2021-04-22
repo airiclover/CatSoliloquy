b466a5a1a901b0019f03aa31a2ba5675fa6edb5e
airi <airi-love-clover@rakuten.jp>
2021-04-22 23:00:29 +0900

    💄 Update UI

--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -1,35 +1,5 @@
 import Link from "next/link";
 
-// export const getStaticProps = async () => {
-//   try {
-//     const res = await fetch(`${process.env.BASE_URL}/rcms-api/1/blog`);
-//     const data = await res.json();
-//     console.log(data);
-
-//     return {
-//       props: { data }
-//     }
-//   } catch (e) {
-//     console.log(e.message);
-//   }
-// };
-
-import axios from "axios";
-
-// axios
-//   .get(process.env.BASE_URL + "/rcms-api/1/blog")
-//   .then(function (response) {
-//     console.log(response);
-//   });
-
-// https://jsonplaceholder.typicode.com/posts/1
-// export async function getStaticProps() {
-//   const res = await fetch(process.env.BASE_URL + "/rcms-api/1/blog");
-//   const posts = await res.json();
-//   console.log(posts);
-//   return { props: { posts } };
-// }
-
 export async function getStaticProps() {
   const res = await fetch(`${process.env.BASE_URL}/rcms-api/1/blog`);
   const data = await res.json();
