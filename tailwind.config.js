94aa3072764f7a390a6da81ae91a5b2d480e5c0c
airi <airi-love-clover@rakuten.jp>
2021-04-24 00:14:27 +0900

    💄 Add theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: ["./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        gray: colors.trueGray,
+      },
+    },
   },
   variants: {
     extend: {},
