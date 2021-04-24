9d5422cf78a7c0830afb7b98bf69e54c5622d8f2
airi <airi-love-clover@rakuten.jp>
2021-04-24 19:37:42 +0900

    ✨ profile file追加

---
+++ src/components/profile.jsx
@@ -0,0 +1,53 @@
+import Image from "next/image";
+
+const SNSES = [
+  { href: "https://twitter.com/cloverlovexxx", src: "twitter.svg" },
+  { href: "https://github.com/airiclover", src: "github.svg" },
+];
+
+export function Profile() {
+  return (
+    <div className="py-10 px-6 bg-gray-100 text-center">
+      <Image
+        src="/img/clover.jpeg"
+        alt="blogIMG"
+        width={120}
+        height={120}
+        objectFit="cover"
+        loading="eager"
+        priority
+        className="rounded-full"
+      />
+
+      <div className="my-2">
+        <div className="text-3xl font-extrabold tracking-wider ">airi</div>
+        {SNSES.map((sns) => (
+          <a
+            href={sns.href}
+            key={sns.src}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-block p-1"
+          >
+            <Image
+              src={`/img/${sns.src}`}
+              alt={sns.src}
+              width={24}
+              height={24}
+              loading="eager"
+              priority
+            />
+          </a>
+        ))}
+      </div>
+
+      <p className="mx-auto w-5/6 text-sm text-left">
+        ああああああああああああああああああああああああああああああああああああああああああああああああああ。
+        <br />
+        あああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ。
+        <br />
+        あああああああああああああああああああああああああああああああ。
+      </p>
+    </div>
+  );
+}
