75920d8915ea7777ea3984d73468bf1c9217cbba
airi <airi-love-clover@rakuten.jp>
2021-04-26 00:05:58 +0900

    💄 UI変更

--- src/layouts/blog.jsx
+++ src/layouts/blog.jsx
@@ -1,5 +1,3 @@
 export function BlogLayout({ children }) {
-  return (
-    <div className="m-6  grid grid-cols-2 gap-y-5 gap-x-4">{children}</div>
-  );
+  return <div className="p-6 grid grid-cols-2 gap-y-5 gap-x-4">{children}</div>;
 }
