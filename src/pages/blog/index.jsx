f4761445ce2cd8be3bc762e3722cfb74fed8fdb4
airi <airi-love-clover@rakuten.jp>
2021-04-26 00:49:18 +0900

    🎨 不要なコード削除

--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -26,32 +26,3 @@ export default function Blog(props) {
     </>
   );
 }
-
-// import { Card } from "src/components/card";
-// import { BlogLayout } from "src/layouts/blog";
-// import { MainLayout } from "src/layouts/main";
-
-// export async function getStaticProps() {
-//   const res = await fetch(`${process.env.BASE_URL}/rcms-api/1/blog`);
-//   const data = await res.json();
-
-//   return {
-//     props: {
-//       data,
-//     },
-//     revalidate: 60,
-//   };
-// }
-
-// export default function Blog(props) {
-//   return (
-//     <>
-//       <MainLayout>
-//         <BlogLayout>
-//           {/* <Card data={props.data} /> */}
-//           <Card pass="blog" data={props.data} />
-//         </BlogLayout>
-//       </MainLayout>
-//     </>
-//   );
-// }
