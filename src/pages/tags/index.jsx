739b60419e3206a2f1d13bab114eb5fcffe34d2c
airi <airi-love-clover@rakuten.jp>
2021-04-28 01:12:10 +0900

    💄 一部レイアウト変更

--- src/pages/tags/index.jsx
+++ src/pages/tags/index.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import { MainLayout } from "src/layouts/main";
 
@@ -16,25 +17,76 @@ export async function getStaticProps() {
 export default function Tags(props) {
   const { data } = props;
   const lists = data.list;
+  console.log(lists);
 
   return (
     <>
       <MainLayout>
-        <div className="p-6 flex flex-col">
-          <h1 className="text-2xl">タグ一覧</h1>
-          <ul className="py-4 pl-4">
-            {lists.map((list) => (
-              <li key={list.tag_id} className="m-1">
-                <Link href={`/tags/${list.tag_id}`}>
-                  {/* <Link href={`/categories/${list.contents_type}`}> */}
+        <div className="py-3 px-6">
+          <div className="flex">
+            <Image
+              src="/img/tag.svg"
+              alt="tagIMG"
+              width={28}
+              height={28}
+              loading="eager"
+              priority
+            />
+            <h1 className="text-2xl">タグ一覧</h1>
+          </div>
 
-                  <a className="py-1 px-2 bg-gray-800 text-xs text-white rounded">
-                    {list.tag_nm}
-                  </a>
-                </Link>
-              </li>
-            ))}
-          </ul>
+          <div className="p-3">
+            <div className="py-3 border-b-2">
+              <h2>プログラミング</h2>
+              <ul className="flex flex-wrap">
+                {lists.map((list) => (
+                  <li key={list.tag_id}>
+                    {list.tag_category_id === 5 && (
+                      <Link href={`/tags/${list.tag_id}`}>
+                        <a className="m-1 py-1 px-2 bg-gray-800 text-xs text-white rounded  inline-block">
+                          {list.tag_nm}
+                        </a>
+                      </Link>
+                    )}
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            <div className="py-3 border-b-2">
+              <h2>旅行</h2>
+              <ul className="flex flex-wrap">
+                {lists.map((list) => (
+                  <li key={list.tag_id}>
+                    {list.tag_category_id === 6 && (
+                      <Link href={`/tags/${list.tag_id}`}>
+                        <a className="m-1 py-1 px-2 bg-gray-800 text-xs text-white rounded inline-block">
+                          {list.tag_nm}
+                        </a>
+                      </Link>
+                    )}
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            <div className="py-3 border-b-2">
+              <h2>ダイアリー</h2>
+              <ul className="flex flex-wrap">
+                {lists.map((list) => (
+                  <li key={list.tag_id}>
+                    {list.tag_category_id === 7 && (
+                      <Link href={`/tags/${list.tag_id}`}>
+                        <a className="m-1 py-1 px-2 bg-gray-800 text-xs text-white rounded inline-block">
+                          {list.tag_nm}
+                        </a>
+                      </Link>
+                    )}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
         </div>
       </MainLayout>
     </>
