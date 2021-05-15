0716eff48619182151b94e2d7faa07f1a3816292
airi <airi-love-clover@rakuten.jp>
2021-05-15 22:21:55 +0900

    ⚗️ Perform experiments.

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -15,11 +15,6 @@ class MyDocument extends Document {
             name="description"
             content="「Cat's Soliloquy」。意味は猫の独り言。フロントエンド学習者による気まぐれブログです。プログラミングの学習記録や趣味について書いています。"
           />
-          <script
-            data-ad-client="ca-pub-5921811975278816"
-            async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-          ></script>
         </Head>
         <body className="dark:bg-gray-700">
           <Main />
